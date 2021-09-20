4108858f4aa70c342af5ba4935430b0893b73a08
abjj1999 <abdullahalhilfi21@gmail.com>
2021-09-20 13:49:24 -0500

    finished home Page

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from './navbar'
+export { default as HomePage } from './Homepage'
+export { default as Exchanges } from './Exchanges'
+export { default as Crypto } from './Crypto'
+export { default as CryptoDetails } from './CryptoDetails'
+export { default as News } from './news'
---
+++ src/components/news.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const news = () => {
+    return (
+        <div>
+            News
+        </div>
+    )
+}
+
+export default news
