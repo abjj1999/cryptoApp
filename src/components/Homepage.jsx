e63baa5c7ff6a3c3a14561261c14dceb5f13910f
abjj1999 <abdullahalhilfi21@gmail.com>
2021-10-13 13:10:30 -0500

    finished the homepage, news, crypto pages

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,7 +7,7 @@ import { Crypto, News } from '../components'
 const { Title } = Typography;
 
 const Homepage = () => {
-    const { data, isFetching } = useGetCryptosQuery();
+    const { data, isFetching } = useGetCryptosQuery(10);
     const globalstats = data?.data?.stats;
     if(isFetching) return 'loading...';
     console.log(data);
